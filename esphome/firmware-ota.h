7246799bbb2ad44aaa327c29da67db287cfbc9d1
Matt Brown <matt@mattb.net.nz>
2023-05-16 20:00:19 +1200

    Add basic Firmware OTA via MQTT msg PoC

---
+++ esphome/firmware-ota.h
@@ -0,0 +1,76 @@
+#include <esp_https_ota.h>
+#include <esp_partition.h>
+#include <esp_ota_ops.h>
+#include <esp_task_wdt.h>
+
+// LetsEncrypt R3 - expires 2025.
+const char* cacertpem = "-----BEGIN CERTIFICATE-----\n\
+MIIFFjCCAv6gAwIBAgIRAJErCErPDBinU/bWLiWnX1owDQYJKoZIhvcNAQELBQAw\n\
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n\
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMjAwOTA0MDAwMDAw\n\
+WhcNMjUwOTE1MTYwMDAwWjAyMQswCQYDVQQGEwJVUzEWMBQGA1UEChMNTGV0J3Mg\n\
+RW5jcnlwdDELMAkGA1UEAxMCUjMwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK\n\
+AoIBAQC7AhUozPaglNMPEuyNVZLD+ILxmaZ6QoinXSaqtSu5xUyxr45r+XXIo9cP\n\
+R5QUVTVXjJ6oojkZ9YI8QqlObvU7wy7bjcCwXPNZOOftz2nwWgsbvsCUJCWH+jdx\n\
+sxPnHKzhm+/b5DtFUkWWqcFTzjTIUu61ru2P3mBw4qVUq7ZtDpelQDRrK9O8Zutm\n\
+NHz6a4uPVymZ+DAXXbpyb/uBxa3Shlg9F8fnCbvxK/eG3MHacV3URuPMrSXBiLxg\n\
+Z3Vms/EY96Jc5lP/Ooi2R6X/ExjqmAl3P51T+c8B5fWmcBcUr2Ok/5mzk53cU6cG\n\
+/kiFHaFpriV1uxPMUgP17VGhi9sVAgMBAAGjggEIMIIBBDAOBgNVHQ8BAf8EBAMC\n\
+AYYwHQYDVR0lBBYwFAYIKwYBBQUHAwIGCCsGAQUFBwMBMBIGA1UdEwEB/wQIMAYB\n\
+Af8CAQAwHQYDVR0OBBYEFBQusxe3WFbLrlAJQOYfr52LFMLGMB8GA1UdIwQYMBaA\n\
+FHm0WeZ7tuXkAXOACIjIGlj26ZtuMDIGCCsGAQUFBwEBBCYwJDAiBggrBgEFBQcw\n\
+AoYWaHR0cDovL3gxLmkubGVuY3Iub3JnLzAnBgNVHR8EIDAeMBygGqAYhhZodHRw\n\
+Oi8veDEuYy5sZW5jci5vcmcvMCIGA1UdIAQbMBkwCAYGZ4EMAQIBMA0GCysGAQQB\n\
+gt8TAQEBMA0GCSqGSIb3DQEBCwUAA4ICAQCFyk5HPqP3hUSFvNVneLKYY611TR6W\n\
+PTNlclQtgaDqw+34IL9fzLdwALduO/ZelN7kIJ+m74uyA+eitRY8kc607TkC53wl\n\
+ikfmZW4/RvTZ8M6UK+5UzhK8jCdLuMGYL6KvzXGRSgi3yLgjewQtCPkIVz6D2QQz\n\
+CkcheAmCJ8MqyJu5zlzyZMjAvnnAT45tRAxekrsu94sQ4egdRCnbWSDtY7kh+BIm\n\
+lJNXoB1lBMEKIq4QDUOXoRgffuDghje1WrG9ML+Hbisq/yFOGwXD9RiX8F6sw6W4\n\
+avAuvDszue5L3sz85K+EC4Y/wFVDNvZo4TYXao6Z0f+lQKc0t8DQYzk1OXVu8rp2\n\
+yJMC6alLbBfODALZvYH7n7do1AZls4I9d1P4jnkDrQoxB3UqQ9hVl3LEKQ73xF1O\n\
+yK5GhDDX8oVfGKF5u+decIsH4YaTw7mP3GFxJSqv3+0lUFJoi5Lc5da149p90Ids\n\
+hCExroL1+7mryIkXPeFM5TgO9r0rvZaBFOvV2z0gp35Z0+L4WPlbuEjN/lxPFin+\n\
+HlUjr8gRsI3qfJOQFy/9rKIJR0Y/8Omwt/8oTWgy1mdeHmmjk7j1nYsvC9JSQ6Zv\n\
+MldlTTKB3zhThV1+XWYp6rjd5JW1zbVWEkLNxE7GJThEUG3szgBVGP7pSWTUTsqX\n\
+nLRbwHOoq7hHwg==\n\
+-----END CERTIFICATE-----\n";
+
+void UpgradeFirmware(const char *url) {
+    // Expand the watchdog time as the flash write is slow.
+    #if ESP_IDF_VERSION_MAJOR >= 5
+        esp_task_wdt_config_t wdtc;
+        wdtc.timeout_ms = 60000;
+        wdtc.idle_core_mask = 0;
+        wdtc.trigger_panic = false;
+        esp_task_wdt_reconfigure(&wdtc);
+    #else
+        esp_task_wdt_init(60, false);
+    #endif
+
+    esp_http_client_config_t config = {
+        .url = url,
+        .cert_pem = cacertpem,
+        .transport_type = HTTP_TRANSPORT_OVER_SSL,
+    };
+    esp_err_t ret = esp_https_ota(&config);
+    ESP_LOGI("ota", "Starting firmware update");
+    if (ret == ESP_OK) {
+        esp_restart();
+        return // never reached in theory.
+    } else {
+        ESP_LOGE("ota", "Firmware upgrade failed");
+    }
+
+    // Set the WDT back to the configured timeout if the upgrade failed.
+    #if ESP_IDF_VERSION_MAJOR >= 5
+        wdtc.timeout_ms = CONFIG_ESP_TASK_WDT_TIMEOUT_S;
+        esp_task_wdt_reconfigure(&wdtc);
+    #else
+        esp_task_wdt_init(CONFIG_ESP_TASK_WDT_TIMEOUT_S, false);
+    #endif
+}
+
+void FirmwareInfo() {
+    const esp_partition_t *part_info = esp_ota_get_running_partition();
+    ESP_LOGI("ota", "Firmware (%s) running from %s", App.get_compilation_time().c_str(), part_info->label);
+}
