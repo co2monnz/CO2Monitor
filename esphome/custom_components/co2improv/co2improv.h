44009389d47d7626f5ee82fa099de5407a6e0e5e
Matt Brown <matt@mattb.net.nz>
2023-06-21 09:42:39 +1200

    Handle WiFi/BLE adv state in one place
    We were forgetting to re-enable WiFi when the user cancelled
    provisioning mode, so centralise state handling in one place.

--- esphome/custom_components/co2improv/co2improv.h
+++ esphome/custom_components/co2improv/co2improv.h
@@ -40,8 +40,7 @@ public:
     // Check if improv succeeded
     if (wc->is_connected()) {
       ESP_LOGD(TAG, "Improv looks to have succeeded! Leaving improve mode.");
-      this->set_timeout(STOP_TIMER, 2000, stopAdvertising);
-      set_state(false);
+      set_state(false, 2000);
       return;
     }
     // Check for timeout
@@ -50,20 +49,37 @@ public:
     if (ageS > 120) {
       // Give up.
       ESP_LOGD(TAG, "Improv timed out after 120s. Disabling advertisements!");
-      stopAdvertising();
       set_state(false);
-      wc->enable(); // re-enable WiFi.
     }
   }
 
   void set_state(bool s) {
+    this->set_state(s, -1);
+  }
+
+  void set_state(bool s, int stop_delay) {
     inImprov = s;
     this->publish_state(inImprov);
     if (this->display != NULL) {
       this->display->set_improv(inImprov);
     }
+    // Trigger actions based on state
     if (inImprov) {
       improvStart = esp_timer_get_time();
+      // Turn off WiFi (improv doesn't seem to work if its already active)
+      wc->disable();
+      // Start improv and BLE advertisements
+      esp32_improv::global_improv_component->start();
+      esp32_ble::global_ble->get_advertising()->start();
+    } else {
+      // Stop BLE advertisements
+      if (stop_delay != -1) {
+        this->set_timeout(STOP_TIMER, stop_delay, stopAdvertising);
+      } else {
+        stopAdvertising();
+      }
+      // Make sure WiFi is re-enabled
+      wc->enable();
     }
   }
 
@@ -77,18 +93,12 @@ public:
     }
     if (inImprov) {
       ESP_LOGD(TAG, "Turning off improv!");
-      stopAdvertising();
       set_state(false);
       return;
     }
     // Make sure we're not about to cancel on a timer
     this->cancel_timeout(STOP_TIMER);
-    // Turn off WiFi (improv doesn't seem to work if its already active)
-    wc->disable();
-    // Start improv
     ESP_LOGD(TAG, "Starting improv!");
-    esp32_improv::global_improv_component->start();
-    esp32_ble::global_ble->get_advertising()->start();
     set_state(true);
   }
 
